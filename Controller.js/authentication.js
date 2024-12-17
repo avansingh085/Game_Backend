722ce9ed9e2d269dd1655c18cce4373ba3ca4f1b
Avan Singh <avansingh085@gmail.com>
2024-12-17 18:00:45 +0530

    ok

--- Controller.js/authentication.js
+++ Controller.js/authentication.js
@@ -1,11 +1,47 @@
+const Users=require('./Schema/UsersSchema.js');
+const jwt=require('jsonwebtoken');
+const generateToken=async (_id)=>{
+  return jwt.sign({id},JWT_SECRET,{ expiresIn: '30m' });
+}
+const register=async (req,res)=>{
+    try{
+       const {username,email,password}=req.body;
+       if(!username||!email||!password)
+       {
+          return res.status(401).send({success:false,result:"invalid email or password,user"});
+       }
+       let isExistUser=await Users.findOne({email});
+       if(isExistUser)
+       {
+        return res.status(500).send({success:false,result:"user alredy exist"});
+       }
+       else
+       {
+            let user=new Users.create({username,password,email});
+            await  user.save();
+            return res.status(200).send({success:true,token:generateToken(user._id),result:"you are successfully register"});
+       }
+    }
+    catch(err){
+         return res.status(400).send({success:false,result:"internal server error"});
+    }
+}
 const login=async (req,res)=>{
    try{
-         let {username,password}=req.body;
-         if(!username||!password)
+         let {email,password}=req.body;
+         if(!email||!password)
             return res.status(400).send({success:false,result:"please fill full required details"});
-        
+        let user=await Users.findOne({email})
+        if(!user&&user.matchPassowrd(password))
+        {
+           return res.status(200).send({success:true,result:"you are successfully Login !",token:generateToken(user._id)})
+        }
+        else{
+            return res.status(401).send({sucess:false,result:"invalid email or password"});
+        }
    }
    catch(err){
-
+          return res.status(500).send({sucess:false,result:"internal server error"});
    }
 }
+module.exports={login,register};
--- Schema/UsersSchema.js
+++ Schema/UsersSchema.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
 const userSchema=new mongoose.Schema({
     username:{type:String},
     email:{type:String,unique:true,required:true},
@@ -11,5 +12,15 @@ const userSchema=new mongoose.Schema({
     totalWin:{type:Number,default:0},
     friends:[{email:{type:String,unique:true,required:true}}]
 });
+
+userSchema.methods.matchPassword=async function (enteredPassword){
+    return bcrypt.compare(enteredPassword,this.password);
+}
+userSchema.pre('save',async function (next){
+    if(!(this.isModified('password')))
+        return next();
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt);
+})
 const createUserSchema=new mongoose.model('usersSchema',userSchema);
 module.exports=createUserSchema;
