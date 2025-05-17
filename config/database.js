891c12dd3b678abd275497f7520b04ed15b7de58
Avan <avansingh085@gmail.com>
2025-05-18 02:02:04 +0530

    update

--- config/database.js
+++ config/database.js
@@ -3,13 +3,16 @@ const dotenv=require('dotenv');
 dotenv.config();
 const connectDB=async ()=>{
     try{
+       
          await mongoose.connect(process.env.MONGODB_URL);
          console.log("databace connect successfully!");
     }
     catch(err)
     {
-          console.log("err to connect databace ");
+          console.log("err to connect database ");
           process.exit(1);
     }
 }
+
 module.exports=connectDB;
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const verifyToken=require('../middlewares/authMiddleware');
 const router = express.Router();
 const { updateProfile, getLeaderBoard, updateScore } = require('../controllers/userController');
 
-router.post('/updateProfile', updateProfile);
+router.post('/updateProfile',verifyToken, updateProfile);
 router.get('/getLeaderboard', getLeaderBoard);
-router.post("/updateScore",updateScore);
+router.post("/updateScore",verifyToken,updateScore);
 
 module.exports = router;
