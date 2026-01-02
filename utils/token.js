3d95d64a74e33aa8a81746c59a8678672f6b167c
Avan <avansingh085@gmail.com>
2026-01-03 01:49:21 +0530

    update

--- utils/token.js
+++ utils/token.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const generateAccessToken=(user)=>{
     return jwt.sign({id:user._id},process.env.ACCESS_TOKEN_SECRETE,{expiresIn:'2m'});
 }
