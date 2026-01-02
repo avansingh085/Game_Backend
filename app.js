f1734546c44fd7ad9cce87a0dd72988697f0e0c7
Avan <avansingh085@gmail.com>
2026-01-03 00:51:15 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandler);
-app.use(cors({ origin:'https://game-application-blond.vercel.app' }));
+app.use(cors({ 
+    origin: 'https://game-application-blond.vercel.app',
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
