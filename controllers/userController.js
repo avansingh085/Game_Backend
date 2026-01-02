9e7db71b8379e91073780785cc5e0a1eb31a02d8
Avan <avansingh085@gmail.com>
2026-01-03 03:09:08 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,7 +58,7 @@ const updateScore = async (req, res) => {
 const getUserProfile = async (req, res) => {
     try {
         const userId = req.userId; 
-        const user = await Users.findById(userId).select("-password"); 
+        const user = await User.findById(userId).select("-password"); 
 
         if (!user) {
             return res.status(404).json({ success: false, result: "User not found" });
